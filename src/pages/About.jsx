b126ace777cc4b6f5f09494d37ec9ccef061f158
Reesejones1989 <mauricejonesjr@gmail.com>
2025-02-16 13:05:04 -0500

    Updated About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,10 @@ export default function About(){
             <p>
                 This app is to bring a place to search for tv show and movie links without the potential ads all over the place
             </p>
+
+            <Footer>Maurice Jones 2025
+            https://reesejones1989.github.io/Portfolio/
+            </Footer>
             
              </div> 
         
