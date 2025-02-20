906e2e0304d55717868c0b72f6acee3378442f67
Reesejones1989 <mauricejonesjr@gmail.com>
2025-02-20 17:38:41 -0500

    fixed mobile-view

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,16 +9,16 @@ export default function Nav() {
       {/* Wrap the nav links in a div */}
       <div className="nav-links">
         <Link id='Home' to="/">
-          <div>Home</div>
+          <div>Home </div> 
         </Link>
         <Link id='Movies' to="/Movies">
-          <div>Movies</div>
+          <div>Movies </div>
         </Link>
         <Link id='TvShows' to="/TV-Shows">
-          <div>Tv Shows</div>
+          <div>Tv Shows </div>
         </Link>
         <Link id='Anime' to="/Anime">
-          <div>Anime</div>
+          <div>Anime </div>
         </Link>
         <Link id='About' to="/About">
           <div>About</div>
