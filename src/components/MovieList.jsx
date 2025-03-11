2263ad2696efe3b973524582f2a1211eaa1ec9df
Reesejones1989 <mauricejonesjr@gmail.com>
2025-03-11 14:22:10 -0400

    Change of the MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,9 +3,9 @@ import "./MovieList.css";
 import { useState, useEffect } from "react";
 
 const initialMovies = [
-  { id: 1, title: "Love Hurts", year: "2025", poster: "" },  
+  { id: 1, title: "A Complete Unknown", year: "2025", poster: "" },  
   { id: 2, title: "One Of Them Days", year: "2025", poster: "" },
-  { id: 3, title: "A Complete Unknown", year: 2024, poster: "" },
+  { id: 3, title: "Picture This", year: 2024, poster: "" },
   { id: 4, title: "Deadpool & Wolverine", year: 2024, poster: "" },
   { id: 5, title: "Wicked", year: 2024, poster: "" },
   { id: 6, title: "Inside Out 2", year: 2024, poster: "" },
