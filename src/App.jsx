80e0f7daf4c5b122a8ce32a1fa6bab3f39e539d7
Reesejones1989 <48375855+Reesejones1989@users.noreply.github.com>
2025-07-05 18:00:38 -0400

    Fixed Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Nav from './components/Nav&Footer/Nav.jsx';
+import Footer from './components/Nav&Footer/Footer.jsx';
 import './App.css';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import TvShows from './pages/TvShows.jsx';
 import Movies from './pages/Movies.jsx';
-import Anime from './pages/Anime.jsx'
-import Sports from './pages/Sports.jsx'
-import Login from './pages/Login.jsx'
-import Favorites from './pages/Favorite.jsx'
-import LiveTv from './pages/LiveTv.jsx'
+import Anime from './pages/Anime.jsx';
+import Sports from './pages/Sports.jsx';
+import Login from './pages/Login.jsx';
+import Favorites from './pages/Favorite.jsx';
+import LiveTv from './pages/LiveTv.jsx';
 import Test from './components/Test/Test.jsx';
-import Footer from './components/Nav&Footer/Footer.jsx'
-import React from 'react'
-import NewMovieFinder from './assets/NewMovieFinder.jpg'
+import React from 'react';
+import NewMovieFinder from './assets/NewMovieFinder.jpg';
 
 function App() {
   return (
@@ -26,52 +26,46 @@ function App() {
         <div className="logo-container">
           <img 
             src={NewMovieFinder} 
-            alt="Movie Links Movie Finder" height="300" width="300"
+            alt="Movie Links Movie Finder"
+            height="300"
+            width="300"
             className="movie-logo" 
           />
         </div>
 
-        {/* Page Content */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Movies" element={<Movies />} />
-          <Route path="/TV-Shows" element={<TvShows />} />
-          <Route path="/Anime" element={<Anime />} />
-          <Route path="/Sports" element={<Sports />} />
-          <Route path="/LiveTv" element={<LiveTv />} />
-          <Route path="/About" element={<About />} />
-          <Route path='/Login' element={<Login />} />
-          <Route path='/Favorites' element={<Favorites />} />
+        {/* Main Page Content */}
+        <div className="page-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Movies" element={<Movies />} />
+            <Route path="/TV-Shows" element={<TvShows />} />
+            <Route path="/Anime" element={<Anime />} />
+            <Route path="/Sports" element={<Sports />} />
+            <Route path="/LiveTv" element={<LiveTv />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Favorites" element={<Favorites />} />
+            <Route path="/Test" element={<Test />} />
 
+            {/* Redirects & Aliases */}
+            <Route path="/Sports" element={<Navigate to="/Sports" replace />} />
+            <Route path="/Anime" element={<Navigate to="/Anime" replace />} />
+            <Route path="/Movies" element={<Navigate to="/Movies" replace />} />
+            <Route path="/Tv-Shows" element={<Navigate to="/TV-Shows" replace />} />
+            <Route path="/LiveTV" element={<Navigate to="/LiveTv" replace />} />
+            <Route path="/Sport" element={<Navigate to="/Sports" replace />} />
+            <Route path="/TvShow" element={<Navigate to="/TV-Shows" replace />} />
+            <Route path="/TvShows" element={<Navigate to="/TV-Shows" replace />} />
 
-          <Route path="/Sports" element={<Navigate to="/Sports" replace />} />
-          <Route path="/Anime" element={<Navigate to="/Anime" replace />} />
-          <Route path="/Movies" element={<Navigate to="/Movies" replace />} />
-          <Route path="/Tv-Shows" element={<Navigate to="/TV-Shows" replace />} />
-          <Route path="/LiveTV" element={<Navigate to="/LiveTv" replace />}  />
-          <Route path="/Test" element={<Test />} />
-          
+            {/* Catch-all */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
 
-
-
-          <Route path="/Sport" element={<Navigate to="/Sports" replace />} />
-          <Route path="/TvShow" element={<Navigate to="/TV-Shows" replace />} />
-          <Route path="/TvShows" element={<Navigate to="/TV-Shows" replace />} />
-          <Route path="/Movie" element={<Navigate to="/Movies" replace />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-
-
-
-
-        </Routes>
-
-
-
-
-        
+        {/* Footer */}
+        <Footer />
       </div>
     </Router>
-
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,6 @@ export default function Home(){
             <SearchBar />
 
             <br/>
-            <Footer />
         </div> 
         
     )
