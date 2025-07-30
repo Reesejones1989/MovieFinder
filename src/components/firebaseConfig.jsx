5ad0a267254530a3003ffcdb8430a8b35a9722d7
Reesejones1989 <48375855+Reesejones1989@users.noreply.github.com>
2025-07-30 11:43:45 -0400

    updated logic x2

--- src/components/firebaseConfig.jsx
+++ src/components/firebaseConfig.jsx
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
-  const FIREBASEapiKey = import.meta.env.FIREBASE;
+const FIREBASEapiKey = import.meta.env.VITE_FIREBASE_API_KEY;
 
 
 const firebaseConfig = {
