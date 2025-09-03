bd7a993cb8b2a0cb22f4956a83fa4b44f91415cf
Reesejones1989 <48375855+Reesejones1989@users.noreply.github.com>
2025-09-03 15:46:13 -0400

    updated tvshows

--- src/components/hardCodedLists/initialTvShows.jsx
+++ src/components/hardCodedLists/initialTvShows.jsx
@@ -1,5 +1,5 @@
 const initialTvShows = [
-    { id: 1, title: "Severance", year: 2008, poster: "" },
+    { id: 1, title: "House of the Dragon", year: 2022, poster: "" },
     { id: 2, title: "The Righteous Gemstones", year: 2011, poster: "" },
     { id: 3, title: "Last of Us", year: 2016, poster: "" },
     { id: 4, title: "Mayor of Kingstown", year: 2005, poster: "" },
@@ -15,6 +15,8 @@ const initialTvShows = [
     { id: 14, title: "Quarterback", year: "2022", poster: "" },
     { id: 15, title: "South Park", year: "1997", poster: "" },
     { id: 16, title: "Insecure", year: "2017", poster: "" },
+    { id: 17, title: "Severance", year: 2008, poster: "" },
+
     
   
 
