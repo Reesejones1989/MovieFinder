c93d7ee1f8b9e64df8336b3cf5b3bf83480c8529
Reesejones1989 <48375855+Reesejones1989@users.noreply.github.com>
2025-09-03 13:21:50 -0400

    updated se links

--- src/components/SportsList.jsx
+++ src/components/SportsList.jsx
@@ -8,31 +8,31 @@ export default function SportsList() {
 <br></br>
       <h2>Sports Leagues </h2>
       <div className="sports-container">
-        <a href="https://the.streameast.app/nflstreams5" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.streameast.ch/se-nfl-streams/" target="_blank" rel="noopener noreferrer">
           <img src="https://content.sportslogos.net/logos/7/1007/full/dwuw5lojnwsj12vfe0hfa6z47.gif" alt="NFL Logo" />
         </a>
 
-        <a href="https://the.streameast.app/nba/streams4" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.streameast.ch/se-nba-streams/" target="_blank" rel="noopener noreferrer">
           <img src="https://content.sportslogos.net/logos/6/982/full/_national_basketball_association_logo_primary_20182722.png" alt="NBA Logo" />
         </a>
 
-        <a href="https://the.streameast.app/mlb/streams" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.streameast.ch/se-mlb-streams/" target="_blank" rel="noopener noreferrer">
           <img src="https://content.sportslogos.net/logos/4/490/full/4227__major_league_baseball-primary-2019.png" alt="MLB Logo" />
         </a>
 
-        <a href="https://the.streameast.app/cfb/streams2" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.streameast.ch/se-cfb-streams/" target="_blank" rel="noopener noreferrer">
           <img src="https://content.sportslogos.net/logos/85/6894/thumbs/689412712024.gif" alt="CFB Logo" />
         </a>
 
-        <a href="https://the.streameast.app/ncaab/streams" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.streameast.ch/se-ncaab-streams/" target="_blank" rel="noopener noreferrer">
           <img src="https://content.sportslogos.net/logos/85/2536/thumbs/253661192025.gif" alt="College Basketball Logo" />
         </a>
 
-        <a href="https://the.streameast.app/boxingstreams13" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.streameast.ch/se-boxing-streams/" target="_blank" rel="noopener noreferrer">
           <img src="https://c8.alamy.com/comp/2K9YANB/mike-tyson-boxing-boxer-champion-vector-t-shirt-design-red-boxing-gloves-download-it-now-2K9YANB.jpg" alt="Boxing Logo" />
         </a>
 
-        <a href="https://the.streameast.app/mmastreams10" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.streameast.ch/se-ufc-streams/" target="_blank" rel="noopener noreferrer">
           <img src="https://1000logos.net/wp-content/uploads/2017/06/Logo-UFC.png" alt="UFC Logo" />
         </a>
 
