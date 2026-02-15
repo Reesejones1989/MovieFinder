753396763131c4202cf10f820aa8555d888452a2
Reesejones1989 <48375855+Reesejones1989@users.noreply.github.com>
2026-02-15 17:14:44 -0500

    frontend backend cleanup

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,6 +3,8 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL, // adjust if needed
 });
+  console.log(baseURL);
+
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
