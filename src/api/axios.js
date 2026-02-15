d8d93092d07a197cc7862ceec1ea0f77f0e66036
Reesejones1989 <48375855+Reesejones1989@users.noreply.github.com>
2026-02-15 17:29:40 -0500

    quick updates

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // adjust if needed
+  baseURL: BASE_URL, // adjust if needed
 });
-  console.log(baseURL);
 
 
 api.interceptors.request.use((config) => {
