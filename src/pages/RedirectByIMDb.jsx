96a95b87802b867fdce0184871e0dd4b347bfa5b
Reesejones1989 <48375855+Reesejones1989@users.noreply.github.com>
2026-04-05 13:19:18 -0400

    Redirect

--- src/pages/RedirectByIMDb.jsx
+++ src/pages/RedirectByIMDb.jsx
@@ -4,6 +4,12 @@ import { useEffect } from "react";
 export default function RedirectByIMDb() {
   const { imdbID } = useParams();
   const navigate = useNavigate();
+  const isValidIMDb = /^tt\d+$/.test(imdbID);
+
+if (!isValidIMDb) {
+  navigate("/");
+  return;
+}
 
   useEffect(() => {
     async function checkType() {
